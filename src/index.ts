4a9eff057809a0aa567640a5adfed1c9ff2390ab
Paula <paulafernandesbastos@ic.uff.br>
2026-09-15 10:44:27 -0300

    update puppeteer

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,7 @@ client.on('ready', async () => {
 
 client.on('message', async (msg: Message) => {
     if (isUnwantedMessage(msg)) return;
+    console.log('mensagem não foi ignorada')
     const groupId = msg.from;
 
     const args = msg.body?.split(/ +/);
